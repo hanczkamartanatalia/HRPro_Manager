ff19fb75fe90d66d8a58606bb4576baa3a5ec3e9
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-11-26 20:11:05 +0100

    add the EntityService

---
+++ Website/Service/EntityService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Website.Database;
+
+namespace Website.Service
+{
+    public static class EntityService<T> where T : Entities.Entity
+    {
+        private static readonly AppDbContext _context;
+
+        public static T GetById(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Incorrect value. Id should be greater than 0.", nameof(id));
+            }
+
+            T entity = _context.Set<T>().FirstOrDefault(x => EF.Property<int>(x, "Id") == id);
+
+            if (entity == null) { throw new ArgumentNullException($"{nameof(entity)}"); }
+
+            return entity;
+        }
+
+        public static List<T> GetAll() 
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public static void RemoveById(int _id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
