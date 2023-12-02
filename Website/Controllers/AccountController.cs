b2f8add145ee6fd69bd6328db05aa49277e0baaf
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-12-02 23:49:54 +0100

    update -> login; add -> accountService;

--- Website/Controllers/AccountController.cs
+++ Website/Controllers/AccountController.cs
@@ -1,35 +1,31 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Website.Entities;
+using Website.Service;
 using Website.Service.AccountService;
 
 namespace Website.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int id)
         {
-            LoginData loginDb = TempData["LoginData"] as LoginData ?? new LoginData();
-
-            if (loginDb == null)
-            {
-                return RedirectToAction("Login");
-            }
-            return View(loginDb);
+            if (id <= 0) return RedirectToAction("Login");
+            LoginData loginData = EntityService<LoginData>.GetById(id);
+            return View(loginData);
         }
         public IActionResult Login()
         {
             return View();
         }
 
-        [HttpPost]
         public IActionResult ProcessLogin(LoginData model)
         {
             try
             {
                 LoginData loginDb = LoginService.Login(model.Login, model.Password);
-                TempData["LoginData"] = loginDb;
-                return RedirectToAction("Index");
+                int id = loginDb.Id;
+                return RedirectToAction("Index", new { id = id });
             }
             catch (Exception ex)
             {
---
+++ Website/Service/AccountService/AccountService.cs
@@ -0,0 +1,13 @@
+﻿using Website.Entities;
+
+namespace Website.Service.AccountService
+{
+    public static class AccountService
+    {
+        public static bool HasAccess(string expectedRole,LoginData loginData)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
