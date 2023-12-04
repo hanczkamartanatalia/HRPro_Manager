41cd0c1c7213dd8545260b9b0a83d5db6188b870
Kamila Zych <UL0262211@edu.uni.lodz.pl>
2023-12-04 20:48:41 +0100

    Add Summary Report

--- Website/Controllers/RaportController.cs
+++ Website/Controllers/RaportController.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Website.Database;
 using Website.Entities;
+using Website.Models;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Website.Controllers
 {
     public class RaportController : Controller
     {
-        private  AppDbContext _context;
+        private AppDbContext _context;
 
         public RaportController(AppDbContext context)
         {
@@ -18,10 +19,31 @@ namespace Website.Controllers
         {
             return View();
         }
-        public IActionResult GenerateSummaryReport()
+        public IActionResult GenerateSummaryReport(string summaryMonth)
         {
+            DateTime startDate = DateTime.Parse(summaryMonth);
+            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
 
-            return View("SummaryReport");
+            var summaryReport = _context.WorkTimes
+    .Where(wt => wt.WorkingDay >= startDate && wt.WorkingDay <= endDate)
+    .Join(
+        _context.Employments,
+        workTime => workTime.Id_User,
+        employment => employment.Id_User,
+        (workTime, employment) => new UserWorkSummary
+        {
+            UserId = workTime.Id_User,
+            Name = workTime.User.Name,
+            Lastname = workTime.User.LastName,
+            TotalHours = workTime.WorkingHours,
+            Earnings = employment.Rate * workTime.WorkingHours
+        })
+    .ToList();
+
+
+            ViewBag.SummaryReport = summaryReport;
+
+            return View("SummaryReport", summaryReport);
         }
 
         [HttpPost]
@@ -31,7 +53,7 @@ namespace Website.Controllers
             DateTime endDate = startDate.AddMonths(1).AddDays(-1);
 
             User userWorkTime = _context.Users.FirstOrDefault(u => u.Name == user.Name && u.LastName == user.LastName);
-            
+
             List<WorkTime> workTimes = _context.WorkTimes
                 .Where(wt => wt.Id_User == userWorkTime.Id && wt.WorkingDay >= startDate && wt.WorkingDay <= endDate)
                 .OrderBy(wt => wt.WorkingDay)
@@ -49,7 +71,7 @@ namespace Website.Controllers
             ViewBag.TotalHours = totalHours;
             ViewBag.UserName = $"{userWorkTime.Name} {userWorkTime.LastName}";
 
-            return View("GenerateIndividualReport", workTimes);
+            return View("IndividualReport", workTimes);
         }
     }
 }
---
+++ Website/Models/UserWorkSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace Website.Models
+{
+    public class UserWorkSummary
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public decimal TotalHours { get; set; }
+        public decimal Earnings { get; set; } 
+    }
+}
