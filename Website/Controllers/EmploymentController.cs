2cc2fa7b1a24570e25a0dd1831835f7f1a444a51
Kamila Zych <UL0262211@edu.uni.lodz.pl>
2023-12-04 11:46:35 +0100

    Add edit employment, details employment, improve display employment list and add new employment

--- Website/Controllers/EmploymentController.cs
+++ Website/Controllers/EmploymentController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using NuGet.DependencyResolver;
 using Website.Database;
 using Website.Entities;
 using Website.Models.Account;
-using Website.Models.Employment;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Position = Website.Entities.Position;
 
 namespace Website.Controllers
 {
@@ -19,49 +22,83 @@ namespace Website.Controllers
 
         public IActionResult Index()
         {
-            List<EmploymentModel> employmentModels = _context.Employments
+            List<Employment> employments = _context.Employments
                 .Include(e => e.User)
                 .Include(e => e.Manager)
                 .Include(e => e.Position)
-                .Select(e => new EmploymentModel
-                {
-                    Id = e.Id,
-                    EmploymentDate = e.EmploymentDate,
-                    Rate = e.Rate,
-                    User = new User { Name = e.User.Name, LastName = e.User.LastName, Email = e.User.Email },
-                    Manager = new User { Name = e.Manager.Name, LastName = e.Manager.LastName, Email = e.Manager.Email },
-                    Position = new Position { Name = e.Position.Name } // Zakładam, że Position ma właściwość Name
-                })
                 .ToList();
 
-            return View(employmentModels);
+            return View(employments);
         }
         public IActionResult Create()
         {
             return View();
         }
-        public IActionResult Add(EmploymentModel employment)
+        public IActionResult Add(Employment employment)
         {
             int userId = (int)TempData["UserId"];
             User user = _context.Users.FirstOrDefault(i => i.Id == userId);
             User manager = _context.Users.FirstOrDefault(u => u.Name == employment.Manager.Name && u.LastName == employment.Manager.LastName);
             Position position = _context.Positions.FirstOrDefault(p => p.Name == employment.Position.Name);
-            Employment employment1 = new Employment
-            {
-                EmploymentDate = employment.EmploymentDate,
-                User = user,
-                Id_User = user.Id,
-                Manager = manager,
-                Id_Manager = manager.Id,
-                Position = position,
-                Id_Position = position.Id,
-                Rate = employment.Rate
-            };
 
-            _context.Employments.Add(employment1);
+            employment.User = user;
+            employment.Id_User = user.Id;
+            employment.Manager = manager;
+            employment.Id_Manager = manager.Id;
+            employment.Position = position;
+            employment.Id_Position = position.Id;
+
+            _context.Employments.Add(employment);
             _context.SaveChanges();
             _context.Dispose();
             return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int Id)
+        {
+            Employment editEmployment = _context.Employments
+                .Include(e => e.User)
+                .Include(e => e.Manager)
+                .Include(e => e.Position)
+                .FirstOrDefault(e => e.Id == Id);
+
+            return View(editEmployment);
+        }
+        public IActionResult Save(Employment employment)
+        {
+            Employment editEmployment = _context.Employments
+                .Include(e => e.User)
+                .Include(e => e.Manager)
+                .Include(e => e.Position)
+                .FirstOrDefault(e => e.Id == employment.Id);
+
+            User user = _context.Users.FirstOrDefault(i => i.Name == employment.User.Name && i.LastName == employment.User.LastName);
+            User manager = _context.Users.FirstOrDefault(u => u.Name == employment.Manager.Name && u.LastName == employment.Manager.LastName);
+            Position position = _context.Positions.FirstOrDefault(p => p.Name == employment.Position.Name);
+
+            editEmployment.EmploymentDate = employment.EmploymentDate;
+            editEmployment.User = user;
+            editEmployment.Id_User = user.Id;
+            editEmployment.Manager = manager;
+            editEmployment.Id_Manager = manager.Id;
+            editEmployment.Position = position;
+            editEmployment.Id_Position = position.Id;
+            editEmployment.Rate = employment.Rate;
+
+            _context.SaveChanges();
+            _context.Dispose();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int Id)
+        {
+            Employment employment = _context.Employments
+                .Include(e => e.User)
+                .Include(e => e.Manager)
+                .Include(e => e.Position)
+                .FirstOrDefault(e => e.Id == Id);
+
+            _context.Dispose();
+            return View(employment);
         }
     }
 }
