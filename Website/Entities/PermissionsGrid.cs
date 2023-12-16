6217518e2d5cb255ee63e495af1b777b0dad6a65
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-12-16 21:10:21 +0100

    add PermissionsGrid class

---
+++ Website/Entities/PermissionsGrid.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Website.Entities
+{
+    public class PermissionsGrid : Entity
+    {
+        public string Page { get; set; } = default!;
+
+        [ForeignKey(nameof(Id_Role))]
+        public Role Role { get; set; } = default!;
+
+        [ForeignKey(nameof(Role))]
+        public int Id_Role { get; set; } = default!;
+
+        public bool HasAccess { get; set; } = false;
+    }
+}
