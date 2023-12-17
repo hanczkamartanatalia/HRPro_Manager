b3334c28397eafccf21a6daf097c8e8d58d8166c
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-12-17 23:51:06 +0100

    add removing id from path in hasAccess method

--- Website/Service/AccountService/AccessService.cs
+++ Website/Service/AccountService/AccessService.cs
@@ -1,4 +1,5 @@
 ﻿using Website.Entities;
+using Website.Migrations;
 
 namespace Website.Service.AccountService
 {
@@ -6,12 +7,27 @@ namespace Website.Service.AccountService
     {
         public static bool HasAccess(string _path, string? _roleName)
         {
-            PermissionsGrid permission = EntityService<PermissionsGrid>.GetBy("Page", _path) ?? throw new Exception("There is no record in the database establishing access to the specified page: " + _path);
+            string path = RemoveEntityIdFromPath(_path);
+
+            PermissionsGrid permission = EntityService<PermissionsGrid>.GetBy("Page", path) ?? throw new Exception("There is no record in the database establishing access to the specified page: " + path);
             if (permission.Id_Role == null) return true;
 
             Role role = EntityService<Role>.GetBy("Name", _roleName) ?? throw new Exception(_roleName + " don't exist. In database there is no this role name.");
             if (permission.Id_Role <= role.Id) return true;
             return false;
+        }
+
+        private static string RemoveEntityIdFromPath(string input)
+        {
+            if(input.Length <= 1) return input;
+
+            if (char.IsDigit(input[input.Length - 1]))
+            {
+                int lastSlashIndex = input.LastIndexOf('/');
+                return input.Substring(0, lastSlashIndex);
+            }
+            return input;
+
         }
     }
 }
