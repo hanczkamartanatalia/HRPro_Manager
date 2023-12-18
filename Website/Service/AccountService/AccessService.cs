061e75f8a1cd3fbe8a67c0f26d8939551c9fee3e
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-12-18 19:10:53 +0100

    update AccessService

--- Website/Service/AccountService/AccessService.cs
+++ Website/Service/AccountService/AccessService.cs
@@ -13,7 +13,7 @@ namespace Website.Service.AccountService
             if (permission.Id_Role == null) return true;
 
             Role role = EntityService<Role>.GetBy("Name", _roleName) ?? throw new Exception(_roleName + " don't exist. In database there is no this role name.");
-            if (permission.Id_Role <= role.Id) return true;
+            if (permission.Id_Role >= role.Id) return true;
             return false;
         }
 
