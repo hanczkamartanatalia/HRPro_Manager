9f31a0eb37f2f52ca7c2c5917215c33df824edf0
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-12-20 17:48:48 +0100

    remove employment from employee menu; add function in filter

--- Website/Filters/AccessFilter.cs
+++ Website/Filters/AccessFilter.cs
@@ -8,10 +8,28 @@ namespace Website.Filters
     {
         public void OnActionExecuting(ActionExecutingContext context)
         {
-
+            string path = context.HttpContext.Request.Path;
+            string? roleName = context.HttpContext.Session.GetString("R_Name");
+            try
+            {
+                bool access = Service.AccountService.AccessService.HasAccess(path, roleName);
+                if (!access)
+                {
+                    context.Result = new RedirectToActionResult("Index", "Home", null);
+                }
+            }
+            catch
+            {
+                context.Result = new RedirectToActionResult("Index", "Home", null);
+            }
         }
 
         public void OnActionExecuted(ActionExecutedContext context)
+        {
+            ControlAccess(context);
+        }
+
+        private void ControlAccess(ActionExecutedContext context)
         {
             string path = context.HttpContext.Request.Path;
             string? roleName = context.HttpContext.Session.GetString("R_Name");
