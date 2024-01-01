71ebeea7dfff7804d89172c9ceacbbe7006b04e6
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2024-01-01 18:29:41 +0100

    Add accept appliations by admin

--- Website/Controllers/ApproveController.cs
+++ Website/Controllers/ApproveController.cs
@@ -124,5 +124,35 @@ namespace Website.Controllers
 
         }
 
+        public IActionResult AllApplications()
+        {
+            try
+            {
+                var query = from user in _context.Users
+                            join employment in _context.Employments on user.Id equals employment.Id_User
+                            join application in _context.Applications on user.Id equals application.Id_User
+                            join category in _context.Categories on application.Id_Category equals category.Id
+                            where application.Id_Category != 1
+                            select new Approve
+                            {
+                                User = user,
+                                Employment = employment,
+                                Application = application,
+                                Category = category
+                            };
+
+                var result = query.ToList();
+
+                return View(result);
+            }
+
+            catch (Exception ex)
+            {
+                return View("Home");
+            }
+
+
+        }
+
     }
 }
