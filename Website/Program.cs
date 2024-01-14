f252d341f4017f9727744dc3edce4bb30f445bd8
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2024-01-14 01:52:49 +0100

    view part 3

--- Website/Program.cs
+++ Website/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
     options.Cookie.Name = "UserSession";
-    options.IdleTimeout = TimeSpan.FromMinutes(5); 
+    options.IdleTimeout = TimeSpan.FromMinutes(20); 
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 }
